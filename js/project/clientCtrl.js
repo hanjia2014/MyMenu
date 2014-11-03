4a8087d3608afe62129a69409db0adc25c2c12c7
hanjia2014 <hanj@datacom.co.nz>
2014-11-04 09:14:46 +1300

    Init

---
+++ js/project/clientCtrl.js
@@ -0,0 +1,32 @@
+﻿menuApp.controller('clientCtrl', function ($scope, menuService) {
+    $scope.copyRight = "Han Jia " + new Date().getFullYear();
+    //$scope.sendEmail = function () {
+    //    $scope.isEmailSent = false;
+    //    if (checkNullOrEmpty($scope.youName)
+    //        || checkNullOrEmpty($scope.youPhone)
+    //        || checkNullOrEmpty($scope.emailMessage)
+    //        || checkNullOrEmpty($scope.emailAddress)) return;
+
+    //    var emailInstance = {};
+    //    emailInstance.name = $scope.youName;
+    //    emailInstance.phone = $scope.youPhone;
+    //    emailInstance.message = $scope.emailMessage;
+    //    emailInstance.from = $scope.emailAddress;
+
+    //    emailService.send(emailInstance).success(function () {
+    //        $scope.isEmailSent = true;
+    //    }).error(function () { });
+    //};
+    $scope.menuItems = [];
+    menuService.getMenu().success(function (data) {
+        var dataItems = data.menu.items;
+
+        while (dataItems.length) {
+            $scope.menuItems.push(dataItems.splice(0, 3));
+        }
+    });
+
+    function checkNullOrEmpty(value) {
+        return value == null || value == '';
+    };
+});
