a3e731ffcfb273e77cfa577e26cd9bdb86b60f50
hanjia2014 <hanj@datacom.co.nz>
2014-11-14 11:34:27 +1300

    use azure url for service

--- js/project/menuService.js
+++ js/project/menuService.js
@@ -4,26 +4,15 @@
             return $http.get('menu.json');
         },
         getCategories: function () {
-            return $http.get('http://localhost:88/api/category');
+            //return $http.get('http://localhost:88/api/category');
+            return $http.get('http://menuserver.azurewebsites.net/api/category');
         },
         getMenuItems: function () {
-            return $http.get('http://localhost:88/api/menuitem');
+            //return $http.get('http://localhost:88/api/menuitem');
+            return $http.get('http://menuserver.azurewebsites.net/api/menuitem');
         },
         submitOrder: function (order) {
-            //return $http({
-            //    method: 'POST',
-            //    url: 'http://localhost:53137/api/order',
-            //    data: order,
-            //    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            //    transformRequest: function(obj) {
-            //        var str = [];
-                    
-            //        for (var p in obj)
-            //            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            //        return str.join("&");
-            //    }
-            //});
-            return $http.post('http://localhost:53137/api/order', order);
+            return $http.post('http://menuserver.azurewebsites.net/api/order', order);
         },
     });
 });
